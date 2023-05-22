26ed159a607ea4ad66641550511f4c5815ba08c3
Achmad Raja Qodli Zaka <achmadrajaqz@gmail.com>
2023-05-22 17:43:48 +0700

    App.js edit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './views/Header';
+import BookList from './views/BookList';
+import BookDetail from './views/BookDetail';
+import LoginForm from './views/LoginForm';
+import BorrowedBooks from './views/BorrowedBooks';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={BookList} />
+        <Route exact path="/books/:id" component={BookDetail} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/borrowed-books" component={BorrowedBooks} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
